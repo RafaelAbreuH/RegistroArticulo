50f5a832cbfa2337a8c7750feb4a228fbf3ec0dd
Rafael Abreu Hernandez <rafael_abreu5@ucne.edu.do>
2018-05-25 21:31:57 -0400

    Agregando Botones a la ventana

--- Registro_Articulos/Mainform.cs
+++ Registro_Articulos/Mainform.cs
@@ -16,5 +16,15 @@ namespace Registro_Articulos
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
